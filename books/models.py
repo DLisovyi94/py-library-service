8985e0ad02d759afc3673fd71a2fbb34578fa240
Dmytro Lisovyi <kenedy11@ukr.net>
2025-07-08 17:36:20 +0300

    Feat: Add Book to models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
+class Book(models.Model):
+    class CoverType(models.TextChoices):
+        HARD = "HARD", "Hard"
+        SOFT = "SOFT", "Soft"
+
+    Title = models.CharField(max_length=255)
+    Author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=10,
+        choices=CoverType.choices,
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=5,
+        decimal_places=2
+    )
+
+    class Meta:
+        verbose_name_plural = "Books"
+
+
+    def __str__(self):
+        return f"{self.Title} by {self.Author}"
+
+
 # Create your models here.
